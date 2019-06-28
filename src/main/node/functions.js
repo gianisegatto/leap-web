78d00e95fd922f8715d91feff1f62dd3e0dc438a
Giani Segatto <gsegatto@travix.com>
2019-06-28 19:54:43 +0100

    added WebErrorBuilder

--- src/main/node/functions.js
+++ src/main/node/functions.js
@@ -3,9 +3,11 @@
 const AppBuilder = require("./app/AppBuilder");
 const promiseHandler = require("./handler/PromiseHandler");
 const RequestValidator = require("./middleware/validator/RequestValidator");
+const WebErrorBuilder = require("./error/WebErrorBuilder");
 
 exports = module.exports;
 
 exports.AppBuilder = AppBuilder;
 exports.promiseHandler = promiseHandler;
 exports.RequestValidator = RequestValidator;
+exports.WebErrorBuilder = WebErrorBuilder;
