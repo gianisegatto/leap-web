720918ca3a653bd0023cc29770b045a33adcac0b
Giani Segatto <gsegatto@travix.com>
2019-06-28 19:57:25 +0100

    importing join properly

--- src/main/node/middleware/validator/RequestValidator.js
+++ src/main/node/middleware/validator/RequestValidator.js
@@ -1,4 +1,4 @@
-const joi = require("joi");
+const joi = require("@hapi/joi");
 const exceptionBuilder = require("../../error/ExceptionBuilder");
 const errorCode = require("../../error/ErrorCode");
 
