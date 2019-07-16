d6b0528cac52f29b52c3f5b96829bf160b039ca5
Giani Segatto <gsegatto@travix.com>
2019-07-16 22:32:22 +0100

    receiving errorCode before details

--- src/main/node/error/WebErrorBuilder.js
+++ src/main/node/error/WebErrorBuilder.js
@@ -2,7 +2,7 @@ const ErrorBuilder = require("leap-core").ErrorBuilder;
 
 class WebErroBuilder {
     
-    static build(source, message, details, errorCode) {
+    static build(source, message, errorCode, details) {
         const error = ErrorBuilder.build(source, message, details);
         error.type = "WEB";
         error.errorCode = errorCode;
