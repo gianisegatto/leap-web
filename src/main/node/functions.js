2000ad3e989becd9c6719a777dd6ab9cee6c4944
Giani Segatto <gsegatto@travix.com>
2019-07-16 22:27:51 +0100

    exposing ErrorCode

--- src/main/node/functions.js
+++ src/main/node/functions.js
@@ -4,6 +4,7 @@ const AppBuilder = require("./app/AppBuilder");
 const promiseHandler = require("./handler/PromiseHandler");
 const RequestValidator = require("./middleware/validator/RequestValidator");
 const WebErrorBuilder = require("./error/WebErrorBuilder");
+const ErrorCode = require("./error/ErrorCode");
 
 exports = module.exports;
 
@@ -11,3 +12,4 @@ exports.AppBuilder = AppBuilder;
 exports.promiseHandler = promiseHandler;
 exports.RequestValidator = RequestValidator;
 exports.WebErrorBuilder = WebErrorBuilder;
+exports.ErrorCode = ErrorCode;
